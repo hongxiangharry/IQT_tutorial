890f2d31a04e7c281293bdfa691cd56ad835542b
Teaching ACCOUNT <ucact21@blaze.cs.ucl.ac.uk>
2020-07-04 21:46:47 +0100

    v1.2

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda 
 rm ~/miniconda.sh
-echo "PATH=$PATH:~/miniconda/bin" >> .bashrc
+echo "PATH=$PATH:~/miniconda/bin" >> ~/.bashrc
 conda env create -f ~/IQT_tutorial/environment.yaml
 source activate iqt
 pip install -r ~/IQT_tutorial/requirements.txt
